6c213bdd8041a3215bc65aa481334c3e85c393ef
foselin <foselinalvarez@gmail.com>
2024-09-06 17:57:42 -0600

    Proyecto Tickets y Angular

--- Contenedor Docker/ProyectoTickets/obj/Debug/net8.0/ProyectoTickets.AssemblyInfo.cs
+++ Contenedor Docker/ProyectoTickets/obj/Debug/net8.0/ProyectoTickets.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoTickets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdc9b9bace4fdda2e7428b6cfad6fcbe6dc5eff0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoTickets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoTickets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

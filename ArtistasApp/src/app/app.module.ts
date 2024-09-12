a509c480f856dd37bce54fc2dfe159c69b69d8b5
Val435 <ia.valentina@ufg.edu.sv>
2024-09-12 14:13:53 -0600

    Add Pantalla Principal

--- ArtistasApp/src/app/app.module.ts
+++ ArtistasApp/src/app/app.module.ts
@@ -3,21 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; // Asegúrate de importar HttpClientModule
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ArtistasListComponent } from './artistas-list/artistas-list.component';
+import { ArtistasListComponent } from './cliente/artistas-list/artistas-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PaginaPrincipalComponent } from './cliente/pagina-principal/pagina-principal.component';
 
 @NgModule({
 
    // Declaramos los componentes que usamos en nuestra app
   declarations: [
     AppComponent,
-    ArtistasListComponent
+    ArtistasListComponent,
+    PaginaPrincipalComponent
   ],
 
    // Importamos los módulos necesarios
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,   // Necesario para hacer peticiones HTTP
+    HttpClientModule,// Necesario para hacer peticiones HTTP
+    BrowserAnimationsModule,   
   ],
   providers: [],
   bootstrap: [AppComponent]
